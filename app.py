fa13863e872cf92424cdedb8cedb6bb820471195
RuabeeTu <2407007304@qq.com>
2025-02-04 22:17:03 +0800

    fix （修复无secret_key导致的无法启动的问题）

--- app.py
+++ app.py
@@ -1,2 +1,9 @@
 from flask import Flask, render_template, request ,blueprints
+from admin.routes import admin
+import secrets
 app = Flask(__name__)
+app.secret_key=secrets.token_urlsafe(64)
+app.register_blueprint(admin, url_prefix='/admin')
+
+if __name__ == '__main__':
+    app.run(debug=True)
