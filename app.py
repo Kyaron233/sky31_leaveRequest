fadbcfd59bda9c9e971fe03e93ba1606cab7c60f
RuabeeTu <2407007304@qq.com>
2025-02-10 10:26:13 +0800

    prof (尝试使用全局数据库连接的方式来配置)

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
 from flask import Flask, render_template, request ,blueprints
 from admin.routes import admin
 import secrets
-app = Flask(__name__)
+from setup_db import setup_db
+
+
+app = setup_db()
 app.secret_key=secrets.token_urlsafe(64)
 app.register_blueprint(admin, url_prefix='/admin')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8000)
+
+
---
+++ setup_db/extension.py
@@ -0,0 +1,36 @@
+from flask import Flask, g
+import mariadb
+
+
+def setup_db():
+    app = Flask(__name__)
+
+    # 数据库配置
+    app.config['DB_CONFIG'] = {
+        'host': 'localhost',
+        'port': 3306,
+        'user': 'root',
+        'password': '240700',
+        'database': 'sky31Employees',
+        'autocommit': True
+    }
+
+    # 请求前自动创建连接
+    @app.before_request
+    def before_request():
+        try:
+            g.conn = mariadb.connect(**app.config['DB_CONFIG'])
+            g.cursor = g.conn.cursor()
+        except mariadb.Error as e:
+            print(f"数据库错误 {e}")
+            return "数据库错误", 500
+
+    # 请求后自动关闭连接
+    @app.teardown_request
+    def teardown_request(exception=None):
+        if hasattr(g, 'cursor'):
+            g.cursor.close()
+        if hasattr(g, 'conn'):
+            g.conn.close()
+
+    return app
