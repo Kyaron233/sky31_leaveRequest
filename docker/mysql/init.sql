3edd03e96e397a1931c1269f09305c907c76e6af
RuabeeTu <2407007304@qq.com>
2025-02-10 17:40:59 +0800

    prof （表结构改进） - 优化了事件和请假者的逻辑

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -10,13 +10,12 @@ create table if not exists admin(
     name VARCHAR(255) NOT NULL,
     pswd_hash VARCHAR(255) NOT NULL
 );
-create table if not exists student( --记住没用复数。。。。
+create table if not exists student(
     student_id VARCHAR(255) NOT NULL UNIQUE,--学号 登录时的账号也就是学号
     name VARCHAR(255) NOT NULL,
     tel VARCHAR(255) NOT NULL,
-    department VARCHAR(255) NOT NULL,--id是不是没啥必要。。或者说部门名称是不是没啥必要。。
-    role_in_depart VARCHAR(255) NOT NULL,--下面那个方案废弃，。。。。
-    --如果没记错的话，部门内只有正部、副部、干事三个职位，职位等级依次递减，那么我们用0，1，2分别标志这三个职位
+    department VARCHAR(255) NOT NULL,-
+    role_in_depart VARCHAR(255) NOT NULL,--存入职位的官方全称
     pswd_hash VARCHAR(255) NOT NULL,
     id INT AUTO_INCREMENT PRIMARY KEY
 
@@ -24,13 +23,17 @@ create table if not exists student( --记住没用复数。。。。
 
 
 CREATE TABLE IF NOT EXISTS events(
-    event_id INT AUTO_INCREMENT PRIMARY KEY,
-    event_name VARCHAR(255) AUTO_INCREMENT PRIMARY KEY,
-    event_date DATETIME
+    event_id INT AUTO_INCREMENT PRIMARY KEY, --事件的id
+    event_name VARCHAR(255) AUTO_INCREMENT PRIMARY KEY not null, --事件名称
+    event_type VARCHAR(255) NOT NULL,--事件类型
+    event_date DATETIME,--事件预定的日期
+    event_post_date DATETIME,--发布这篇请假请求的时间
+    event_sight_sort VARCHAR(255) --设置事件的可见性，默认值就是当前发布者登录的账号所属于的部门，其他人在搜索的时候按部门搜索，用“全局”代表所有人都能查看
 );
 CREATE TABLE IF NOT EXISTS whoLeave(
+    whoLeave_event VARCHAR(255),--对应请假的事件
     whoLeave_order INT AUTO_INCREMENT PRIMARY KEY,--请假者的次序
-    whoLeave_id INT NOT NULL,
+    whoLeave_id INT NOT NULL, --学号
     whoLeave_name VARCHAR(255) NOT NULL,
 
     leave_reason VARCHAR(255) NOT NULL,--请假原因
