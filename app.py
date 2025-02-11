392c7982c7ccb85d27dbcc77bbd610ed83312a13
RuabeeTu <2407007304@qq.com>
2025-02-12 01:01:34 +0800

    chore:修改以适应gunicorn

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ app = setup_db()
 app.secret_key=secrets.token_urlsafe(64)
 app.register_blueprint(admin, url_prefix='/admin')
 
+# 在服务器部署的代码中使用gunicorn，故删除了这里的启动（不过有点不方便本地调试）
 @app.route('/')
 def hello():
     return "hello!"
