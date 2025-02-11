41cfd9a22fddcbcce41753d92a55f9ef5c36f8e1
RuabeeTu <2407007304@qq.com>
2025-02-11 21:03:01 +0800

    fix:优化表结构 chore:添加示例数据

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,4 +1,5 @@
 CREATE USER 'root'@'localhost' IDENTIFIED BY 'sky31admin';
+CREATE USER 'admin'@'localhost' IDENTIFIED BY 'sky31admin';
 CREATE DATABASE IF NOT EXISTS sky31Employees;
 GRANT ALL PRIVILEGES ON sky31Employees.* TO 'admin'@'localhost';
 FLUSH PRIVILEGES;
@@ -13,9 +14,10 @@ create table if not exists admin(
 create table if not exists student(
     student_id VARCHAR(255) NOT NULL UNIQUE,--学号 登录时的账号也就是学号
     name VARCHAR(255) NOT NULL,
+    isPresent INT default 0, -- 意在标识是否属于主席团成员，部门为其分管的部门，其中主席团成员（团支书等）也标记为1
     tel VARCHAR(255) NOT NULL,
     department VARCHAR(255) NOT NULL,-
-    role_in_depart VARCHAR(255) NOT NULL,--存入职位的官方全称
+    role_in_depart VARCHAR(255) NOT NULL,--存入职位的官方全称，团支书、正主席、部门分管、正部长、副部长、干事
     pswd_hash VARCHAR(255) NOT NULL,
     id INT AUTO_INCREMENT PRIMARY KEY
 
@@ -23,12 +25,13 @@ create table if not exists student(
 
 
 CREATE TABLE IF NOT EXISTS events(
-    event_id INT AUTO_INCREMENT PRIMARY KEY, --事件的id
+    event_id INT AUTO_INCREMENT PRIMARY KEY, --事件的id，用于排序
     event_name VARCHAR(255) AUTO_INCREMENT PRIMARY KEY not null, --事件名称
     event_type VARCHAR(255) NOT NULL,--事件类型
     event_date DATETIME,--事件预定的日期
-    event_post_date DATETIME,--发布这篇请假请求的时间
-    event_sight_sort VARCHAR(255) --设置事件的可见性，默认值就是当前发布者登录的账号所属于的部门，其他人在搜索的时候按部门搜索，用“全局”代表所有人都能查看
+    -- event_post_date DATETIME,--发布这篇请假请求的时间
+    event_department VARCHAR(255) NOT NULL,--事件所属部门,全体事件标记为“全中心”
+    isActive INT NOT NULL default 1 --事件的时效性，1表示正在进行，0表示已经过期
 );
 CREATE TABLE IF NOT EXISTS whoLeave(
     whoLeave_event VARCHAR(255),--对应请假的事件
@@ -36,12 +39,16 @@ CREATE TABLE IF NOT EXISTS whoLeave(
     whoLeave_id INT NOT NULL, --学号
     whoLeave_name VARCHAR(255) NOT NULL,
 
+    related_event INT NOT NULL, --请假表关于哪个事件，用event_id表示
     leave_reason VARCHAR(255) NOT NULL,--请假原因
     check_opinion VARCHAR(255) ,--审批意见，可以是空值
     is_permitted INT DEFAULT 0,--通过与否，0代表未审批，1和-1分别代表同意和不同意
     check_time DATETIME,
-    path_to_image VARCHAR(255)--由于要上传事实图片作为请假材料，故使用此变量记录服务器中上传的图片的位置
+    photo_paths VARCHAR(255), --由于要上传事实图片作为请假材料，故使用此变量记录服务器中上传的图片的位置
+    photo_amount INT NOT NULL
     --记得写上传图片的接口。。。。。
 );
+
+INSERT INTO admin (admin_id,name,pswd_hash) VALUES ('admin','testAdmin','b''$2b$12$DQ2wDn2CRcF9DhJX/MVMsum7K7zSEbKlvX7.l/mHJs1N0yA3ghI8.''')
 
 
