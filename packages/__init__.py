c7c00587cb4474859b3c98604e14b59cad4e45e2
RuabeeTu <2407007304@qq.com>
2025-03-01 10:41:34 +0800

    fix:增加反向映射

--- packages/__init__.py
+++ packages/__init__.py
@@ -16,6 +16,20 @@ department_mapping = {
     "president": "主席团"
 }
 
+department_mapping_reverse = {
+    "媒体运营部": "media",
+    "翼工坊": "workshop",
+    "产品经理与产品运营部": "product",
+    "技术研发部": "tech",
+    "音视频文化工作室": "video",
+    "行政人事部": "HR",
+    "外宣部": "ER",
+    "微信推文部": "wechat",
+    "新闻通讯社": "news",
+    "企划公关部": "CPPR",
+    "设计部": "design",
+    "主席团": "president"
+}
 # 职位参数映射
 role_in_depart_mapping = {
     4:"正主席/团支书",
